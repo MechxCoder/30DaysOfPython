ad92c054ff831f31aa6ed93b5b5ffb6e0ad6e53b
MechxCoder <rutikbarde022@gmail.com>
2024-11-01 15:29:51 +0530

    Explain about print command

--- Day1.py
+++ Day1.py
@@ -1,2 +1,3 @@
 print (" Hello world ")
 # Wohooooo!!! congtatulation for completinng your first  program :)
+# In pythonn 'print' is a function and whatever you write in th syntax this will print in the terminnal or in code runner 
